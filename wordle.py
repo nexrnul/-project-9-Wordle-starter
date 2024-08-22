e7d05b32648545b42b389335be63bf2c71167b44
nexer nul <redpenguin4luca26@gmail.com>
2024-08-22 16:58:17 +0000

    complelted wordle

--- wordle.py
+++ wordle.py
@@ -1,5 +1,4 @@
-print("Let's play wordle! /n Guess the Wordle in 6 tries. Each guess must be a valid 5-letter word. For each guess, a hint will tell you how many letters you've guessed correctly. A G represents a letter in the word and in the correct spot.. A Y represents a letter in the word but in the wrong spot. A - represents a letter not in the word in any spot. \n Guess below! \n")
-
+print("Let's play wordle!\nGuess the Wordle in 6 tries.\nEach guess must be a valid 5-letter word.\nFor each guess, a hint will tell you how many letters you've guessed correctly:\nA G represents a letter in the word and in the correct spot.\nA Y represents a letter in the word but in the wrong spot.\nA _ represents a letter not in the word in any spot.\nGuess below!")
 
 word = "water"
 
@@ -14,13 +13,17 @@ def makeAGuess(guess):
 
     else: 
       hint += "_"
-  return hint
+  print(hint)
+  return(hint)
 
-print(makeAGuess("sport"))
-
-#for i in (0,5):
- #   makeAguess()
-
+for i in range(5):
+  user_guess = input()
+  hint = makeAGuess(user_guess)
+  if hint != "GGGGG":
+    print("you did not solve the puzzle try again")
+  elif hint == "GGGGG":
+    print("congratulations on solving the puzzle")
+    break  
 
 ## TASK A: Define a variable 'word' that holds the correct word for the wordle game
 ## TASK B: Define a function 'makeAGuess()' that passes in a users guess as a parameter
@@ -30,17 +33,10 @@ print(makeAGuess("sport"))
 # TASK F: If the previous condition is fales, check if the current letter of guess is in word at all. If so add the letter "Y" to the hint
 # TASK G: If the previous two conditions are false, add the symbol "-" to the hint
 # TASK H: Return hint
-  
 
 # TASK I: Build a loop that loops 6 times (representing the number of guesses a user has)
-
   # TASK J: Define a variable 'guess'. prompt the user for their 5-letter guess and store it in the variable
-
   # TASK K: Define a variable 'hint' and set the return of makeAGuess(guess) to that variable
-
   # TASK L: Print hint
-
   # TASK M: Check if hint = "GGGGG". If so the user has won. Print a win message and break the loop
-  
-
 # TASK N: After the loop has finished, meaning the user has run out of guesses, check if hint != "GGGGG". If so, the user has lost. Print a lose message. 
