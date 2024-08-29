0723c8550ee2dc743722d0392bc197009472a0f1
nexer nul <redpenguin4luca26@gmail.com>
2024-08-29 16:58:38 +0000

    3 classes sample

--- character.py
+++ character.py
@@ -1,27 +1,36 @@
-class CharacterA:
+class Character:
 
     # init method or constructor
-    def __init__(self, skill_class, name):
+    def __init__(self, skill_class, name, strength, health, exp=1):
         self.skill_class = skill_class
         self.name = name
+        self.health = health
+        self.exp = exp
+        self.strength = strength
 
-    # Sample Method
-    def introcution(self):
-        print('Hello, my name is', self.name)
-        print('I am a', self.skill_class)
+    def introduction(self):
+        print(f"My name is {self.name} and I am a {self.skill_class}")
+
+    def attack(self):
+        print(f"{self.name} attacks for {self.strength*10} damage!")
+
 # Creating different objects
-c1 = CharacterA('Mage', 'A')
-c1.introcution()
 
-class CharacterA:
-    # init method or constructor
-    def __init__(self, skill_class, name):
-        self.skill_class = skill_class
-        self.name = name
-    # Sample Method
-    def introcution(self):
-        print('Hello, my name is', self.name)
-        print('I am a', self.skill_class)
 
-c2 = CharacterA('Warrior', 'B')
-c2.introcution()
+class Mage(Character):
+    def __init__(self, name, strength=8, health=60, exp=1):
+        super().__init__('Mage', name, strength, health, exp)
+
+
+class Ranger(Character):
+
+
+class Cleric(Character):
+
+
+c1 = Character('Mage', 'A', 8, 60)
+c2 = Character('Ranger', 'B', 6, )
+c3 = Character('Cleric', 'C', 4, )
+c1.introduction()
+c2.introduction()
+c3.introduction()
